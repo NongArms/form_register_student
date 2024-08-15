357013e73b7386f3c24e98739bed0695b37f376d
NongArms <amnatpatum@gmail.com>
2024-08-15 20:05:13 +0700

    add input tags

--- handleFunctions/formSend.js
+++ handleFunctions/formSend.js
@@ -4,11 +4,21 @@ function formSend(event) {
     const password = event.target.elements["password"].value;
     const firstname = event.target.elements["firstname"].value;
     const lastname = event.target.elements["lastname"].value;
+    const dateOfBirth = event.target.elements["date_of_birth"].value;
+    const gender = event.target.elements["gender"].value;
+    const email = event.target.elements["email"].value;
+    const height = event.target.elements["height"].value;
+    const weight = event.target.elements["weight"].value;
     const data = {
         username: username,
         password: password,
         firstname: firstname,
         lastname: lastname,
+        dateOfBirth: dateOfBirth,
+        gender: gender,
+        email: email,
+        height: height,
+        weight: weight,
     };
     console.log(data);
 }
